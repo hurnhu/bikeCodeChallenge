bf1cfe882381c20e509c49e8c3147c9011ce3591
Michael LaPan <codethrower@gmail.com>
2024-01-03 09:51:19 -0500

    initial commit 9:51AM EST

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Coding Challenge:
+
+// Using a PHP framework, create a Bicycle Class and instantiate it. It needs some working parts of a bicycle and should be able to be run.
+
+// We will be looking for the following:
+
+//     Code cleanliness
+//     Error handling
+//     OOP paradigms
+var_dump("hello");
+?>
+
